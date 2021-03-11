557518051609577dd18dd2d6b4aa38ea67628c2e
lunghekani <lunghivlanga@gmail.com>
2021-03-11 22:43:46 +0200

    Add project files.

---
+++ 218051864-Lab-Exercise-2-beta/connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _218051864_Lab_Exercise_2_beta
+{
+    class connection
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ stuff/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stuff
+{
+    public class Class1
+    {
+    }
+}
